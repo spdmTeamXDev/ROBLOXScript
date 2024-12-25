ccb5630d770462e35ba00f02fc768b3b0b5d4dbc
Articles-Hub <gianghub56@gmail.com>
2024-12-25 21:20:12 +0700

    Create FarmBob.lua
    Signed-off-by: Articles-Hub <gianghub56@gmail.com>

---
+++ File/FarmBob.lua
@@ -0,0 +1,19 @@
+if game.Players.LocalPlayer.leaderstats.Glove.Value == "God's Hand" then
+game:GetService("ReplicatedStorage").TimestopJump:FireServer()
+game:GetService("ReplicatedStorage").Timestopchoir:FireServer()
+game:GetService("ReplicatedStorage").Timestop:FireServer()
+wait(5)
+fireclickdetector(workspace.Lobby.Replica.ClickDetector)
+wait(0.1)
+repeat task.wait() until game.Players.LocalPlayer.Character
+if not game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+repeat task.wait()
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), workspace.Lobby.Teleport1.TouchInterest.Parent, 0)
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), workspace.Lobby.Teleport1.TouchInterest.Parent, 1)
+until game.Players.LocalPlayer.Character:FindFirstChild("entered")
+end
+wait(0.3)
+for i = 1, 5000 do
+game:GetService("ReplicatedStorage").Duplicate:FireServer(true)
+end
+end 
